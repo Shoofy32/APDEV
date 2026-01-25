60cba3e2d4c5884c15ba6dd0aea928df9986baaf
TheNinjaDude12 <108714992+TheNinjaDude12@users.noreply.github.com>
2026-01-25 21:57:06 +0800

    Add files via upload

--- forumpage/forum.js
+++ forumpage/forum.js
@@ -1,8 +1,13 @@
-const Button = document.querySelectorAll('.like_button')
-function handleClick(like) {
-    
-}
-Button.forEach(element => {
+const LikeButton = document.querySelectorAll('.like_button')
+const DislikeButton = document.querySelectorAll('.dislike_button')
+const post = document.getElementById('post_button');
+
+
+post.addEventListener('click', function() {
+  // Navigate to the new page
+  window.location.href = 'post.html';
+});
+LikeButton.forEach(element => {
  var likeButton = element.children[0]
  
 
@@ -12,6 +17,21 @@ Button.forEach(element => {
     }
     else {
         likeButton.src = 'hand-thumbs-up.svg';
+    }
+   
+ })
+})
+
+DislikeButton.forEach(element => {
+ var DislikeButton = element.children[0]
+ 
+
+ element.addEventListener('click', () => {
+     if(DislikeButton.src.includes("hand-thumbs-down.svg")){
+        DislikeButton.src = 'hand-thumbs-down-fill.svg';
+    }
+    else {
+        DislikeButton.src = 'hand-thumbs-down.svg';
     }
    
  })
