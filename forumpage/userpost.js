b231b16728ae4d5b9a42a1f2384b15648f3200c6
TheNinjaDude12 <108714992+TheNinjaDude12@users.noreply.github.com>
2026-02-07 19:58:07 +0800

    Add files via upload

--- forumpage/userpost.js
+++ forumpage/userpost.js
@@ -4,14 +4,15 @@ const close = document.getElementById('closeReply');
 const postReply = document.getElementById('postReply');
 const textArea = document.getElementById('content');
 const all_posts = document.getElementById('all_posts');
-
-
+var original_post="";
+var current_post = "";
+var user = "";
 postReply.addEventListener("click", () => {
   const content = textArea.value.trim();
   if (!content) return;
 
   reply.classList.remove("open");
-
+if(original_post.length !== 0) {
   all_posts.insertAdjacentHTML("beforeend", `
   <div class="post_container">
     <div class="reply">
@@ -28,28 +29,103 @@ postReply.addEventListener("click", () => {
       </div>
 
       <div class="post_contents">
-       <div class="replying_to_container"><span>Charlie_Kirk said:</span><br><br>
+       <div class="replying_to_container"><span>${user} said:</span><br><br>
                         <p>
-                            Man DLSU pc's are the best!!! How could you not love the dlsu pcs? They have RTX 5090 in them. 
-                        It is so good that my groupmates in INFOM used its CPU to generate AI code!!! Thank you Lasalle Gaming. It is good that my wife remarried me!
-                        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa   
+                           ${original_post}
                         </p>
                     </div>
         <p class="reply_contents">${content}</p>
       </div>
     </div>
+        <div class="interaction_container">
+                    <div> 
+                        <button class="like_button">
+                            <img class="thumbs_up" src="hand-thumbs-up.svg">
+                            <p>0</p>
+                        </button>
+                    </div>
+                    <div> <button class="dislike_button">
+                        <img src="hand-thumbs-down.svg">
+                        <p>0</p>
+                    </button>
+                    </div>
+                    <div><button class="reply_button">Reply</button></div>
+                    
+                    </div> 
   </div>
   `);
 
+}
+else {
+  all_posts.insertAdjacentHTML("beforeend", `
+  <div class="post_container">
+    <div class="reply">
+      <div class="post_info">
+        <img src="kirk.jfif"/>
+
+        <div class="post_user">
+          <h5>Charlie_Kirk</h5>
+        </div>
+
+        <div class="timestamp">
+          <p>&nbsp; just now</p>
+        </div>
+      </div>
+
+      <div class="post_contents">
+       <div class="replying_to_container"><span>${user} said:</span><br><br>
+                        <p>
+                           ${current_post}
+                        </p>
+                    </div>
+        <p class="reply_contents">${content}</p>
+      </div>
+    </div>
+        <div class="interaction_container">
+                    <div> 
+                        <button class="like_button">
+                            <img class="thumbs_up" src="hand-thumbs-up.svg">
+                            <p>0</p>
+                        </button>
+                    </div>
+                    <div> <button class="dislike_button">
+                        <img src="hand-thumbs-down.svg">
+                        <p>0</p>
+                    </button>
+                    </div>
+                    <div><button class="reply_button">Reply</button></div>
+                    
+                    </div> 
+  </div>
+  `);
+
+}
+  
+
   textArea.value = "";
 });
-replyButton.forEach(element => {
-element.addEventListener('click', () => {
-   
-  reply.classList.add("open");
-    })
-})
+all_posts.addEventListener('click', (e) => {
+
+  if (e.target.classList.contains('reply_button')) {
+    const parent = e.target.parentNode.parentNode.parentNode;
+    const contents = parent.querySelector('.post_contents');
+    const info = parent.querySelector('.post_info');
+    var reply_post = contents.querySelector('.reply_contents');
+    var check_post = contents.querySelector("#original_post");
+    user = info.querySelector('.post_user').querySelector('h5').textContent;
+  
+    if(check_post) {
+      original_post = check_post.textContent;
+      current_post = "";
+    } else {
+      original_post = "";
+      current_post = reply_post.textContent;
+    }
+
+    
+    reply.classList.add("open");
+  }
+});
 
 close.addEventListener("click", ()=> {
     reply.classList.remove("open");
