ce214483ee4b32108633afa479e2de8292a3c104
TheNinjaDude12 <108714992+TheNinjaDude12@users.noreply.github.com>
2026-02-11 18:32:25 +0800

    Add files via upload

--- forumpage/forum.js
+++ forumpage/forum.js
@@ -1,9 +1,4 @@
-const post = document.getElementById('post_button');
-const challenge = document.getElementById('challenge_container');
-const close = document.getElementById('closeChallenge');
-const postBet = document.getElementById("postBet");
-const d20 = document.getElementById("d20")
-
+// ===== LOAD NEW POST FROM localStorage =====
 window.addEventListener('DOMContentLoaded', function() {
     loadNewPost();
 });
@@ -170,6 +165,12 @@ function setupButtons() {
         });
     });
 
+
+    // Challenge Button Disabled Here (REMOVE BEFORE SUBMISSION)
+
+    /*
+
+    // Challenge buttons
     document.querySelectorAll('.Challenge').forEach(element => {
         element.addEventListener('click', (e) => {
             e.preventDefault(); 
@@ -177,43 +178,21 @@ function setupButtons() {
         });
     });
 
-    document.querySelectorAll('.Delete').forEach(element=> {
-         element.addEventListener('click', (e) => {
-            var parent_div = element.parentElement.parentElement.parentElement
-            parent_div.remove();
-            console.log(parent_div)
-            e.preventDefault(); 
-           
-        });
-    })
+    */
 
-     document.querySelectorAll('.Edit').forEach(element=> {
-         element.addEventListener('click', (e) => {
-            var parent_div = element.parentElement.parentElement.parentElement
-            var contents = parent_div.querySelector(".post_contents").querySelector("p");
-            var current_content = contents.innerText;
-            contents.innerHTML = `<textarea id="editArea"> ${current_content}</textarea><div id="edit_container">
-            <button id="Save">Save </button>
-            </div>
-        
-            ` ;
-            var textarea = document.getElementById("editArea");
-            var save = document.getElementById("Save");
-            textarea.addEventListener("click", function(e) {
-            e.preventDefault();
-            });
-            save.addEventListener("click", function(e) {
-                e.preventDefault();
-                contents.innerHTML = `<p>${textarea.value} (edited)</p>`
-            });
-            console.log(current_content)
-            e.preventDefault(); 
-           
-        });
-    })
+
 }
 
 
+// Challenge Button Behavior Disabled Here (REMOVE BEFORE SUBMISSION)
+
+
+const post = document.getElementById('post_button');
+const challenge = document.getElementById('challenge_container');
+const close = document.getElementById('closeChallenge');
+const postBet = document.getElementById("postBet");
+
+/*
 close.addEventListener("click", (e) => {
     e.preventDefault();
     e.stopPropagation(); 
@@ -236,7 +215,7 @@ postBet.addEventListener("click", (e) => {
         console.log(challenge.classList);
         let roll = Math.floor(Math.random() * 20);
         document.getElementById("betLikes").value = "";
-       
+        alert(roll);
         console.log(typeof likes);
     } else {
         alert("Please enter a positive number");
