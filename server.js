cd141b314d316897fa20604256dc34e2c95d5ca9
Stefan <stefandomingo17@gmail.com>
2026-03-19 14:51:48 +0800

    saddd

--- server.js
+++ server.js
@@ -21,11 +21,6 @@ app.use(
     secret: "secret-key",
     resave: false,
     saveUninitialized: false,
-    /*cookie: { 
-      secure: false, 
-      httpOnly: true, 
-      maxAge: 24 * 60 * 60 * 1000
-    }*/
   })
 );
 
