e3bbdf9d2e8a5786d080347976af98b957b929eb
TheNinjaDude12 <108714992+TheNinjaDude12@users.noreply.github.com>
2026-04-06 15:59:19 +0800

    Add files via upload

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const sideBarProfile = document.getElementsByClassName("sidebar_profile_button")[0]; // Side bar home button
     const sideBarShop = document.getElementsByClassName("sidebar_shop_button")[0]; // Side bar home button
 
-    const sideBarThreads = document.getElementsByClassName("sidebar_home_button")[2]
     const sideBarDropdowns = document.getElementsByClassName("side_bar_title_container"); // Dropdown titles
+
     const aboutButton = document.getElementsByClassName("about_topic_button")[0];
 
 
@@ -20,15 +20,6 @@ document.addEventListener("DOMContentLoaded", () => {
         window.location.href = "/";
 
     });
-
-
-
-
-    
-    sideBarThreads.addEventListener("click", function () {
-         window.location.href = "/userprofile";
-
-    })
 
     // Add eventlistener to each sidebar dropdown to expand when clicked
     for(let i = 0; i < sideBarDropdowns.length; i ++)
