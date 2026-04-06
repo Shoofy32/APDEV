4fdcce0ba00aa21e3c3eb37b5374c174d3cf1e07
TheNinjaDude12 <108714992+TheNinjaDude12@users.noreply.github.com>
2026-04-06 15:46:34 +0800

    Update sidebar.js

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const sideBarProfile = document.getElementsByClassName("sidebar_profile_button")[0]; // Side bar home button
     const sideBarShop = document.getElementsByClassName("sidebar_shop_button")[0]; // Side bar home button
 
-    const sideBarTrending = document.getElementsByClassName("sidebar_home_button")[1]
     const sideBarThreads = document.getElementsByClassName("sidebar_home_button")[2]
     const sideBarDropdowns = document.getElementsByClassName("side_bar_title_container"); // Dropdown titles
     const aboutButton = document.getElementsByClassName("about_topic_button")[0];
@@ -22,16 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     });
 
-    sideBarTrending.addEventListener("click", () => {
-        
-        if (window.location.pathname === "/") {
-            document.getElementsByClassName("all_posts_popular")[0].scrollIntoView({ behavior: "smooth" });
-        } else {
-            window.location.href = "/?page=1#trending";
-            }
 
-
-    });
 
 
     
